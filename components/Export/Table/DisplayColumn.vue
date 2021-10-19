f020690d2300c918e3c31b4aa83add38a4d9c3a1
AltFreq <altfreq07@gmail.com>
2021-10-20 09:11:12 +1100

    Multiline work around on span for report

--- components/Export/Table/DisplayColumn.vue
+++ components/Export/Table/DisplayColumn.vue
@@ -1,5 +1,12 @@
 <template>
-  <span class="text-body-2 ma-0 pa-0 text-no-wrap">{{ value }}</span>
+  <div>
+    <span
+      v-for="(text, index) in value.split('\n')"
+      :key="index"
+      class="text-body-2 ma-0 pa-0 text-no-wrap"
+      >{{ text }}<br
+    /></span>
+  </div>
 </template>
 <script>
 export default {
