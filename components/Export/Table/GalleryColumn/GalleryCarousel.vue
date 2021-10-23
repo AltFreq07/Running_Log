b8014a73e396fc40e3a6ba1d64faa8881f9efd8a
AltFreq <altfreq07@gmail.com>
2021-10-23 21:46:42 +0000

    Fix up gallery - Hide buttons after 2s - Fit images properly

--- components/Export/Table/GalleryColumn/GalleryCarousel.vue
+++ components/Export/Table/GalleryColumn/GalleryCarousel.vue
@@ -1,21 +1,40 @@
 <template>
   <v-dialog v-model="dialog" width="100%">
-    <v-carousel height="90vh">
+    <v-carousel
+      height="90vh"
+      :hide-delimiter-background="hideControls"
+      :hide-delimiters="hideControls"
+      @change="activateControls"
+    >
       <v-carousel-item
         v-for="(image, i) in array"
         :key="i"
         reverse-transition="fade-transition"
         transition="fade-transition"
       >
-        <v-sheet height="100%" width="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <img :src="image" class="imageFit" />
-          </v-row>
+        <v-sheet height="100%" width="100%" absolute>
+          <v-container style="height: inherit">
+            <v-row
+              style="height: inherit"
+              class="ma-0"
+              align="center"
+              justify="center"
+            >
+              <img :src="image" class="imageFit" />
+            </v-row>
+          </v-container>
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
   </v-dialog>
 </template>
+
+<style>
+.imageFit {
+  max-height: 100%;
+  max-width: 100%;
+}
+</style>
 
 <script>
 import ColumnProps from '~/mixins/ColumnProps.js'
@@ -25,7 +44,23 @@ export default {
   data() {
     return {
       dialog: false,
+      hideControls: false,
+      timer: undefined,
     }
+  },
+  methods: {
+    activateControls() {
+      console.log('hiding controls')
+      this.hideControls = false
+      if (this.timer !== undefined) window.clearTimeout(this.timer)
+      this.timer = setTimeout(
+        function () {
+          this.hideControls = true
+          console.log('hiding controls now')
+        }.bind(this),
+        2000
+      )
+    },
   },
 }
 </script>
--- components/Table/ScreenshotColumn/ScreenshotCarousel.vue
+++ components/Table/ScreenshotColumn/ScreenshotCarousel.vue
@@ -1,34 +1,53 @@
 <template>
   <v-dialog v-model="dialog" width="100%">
-    <v-carousel height="90vh">
+    <v-carousel
+      height="90vh"
+      :hide-delimiter-background="hideControls"
+      :hide-delimiters="hideControls"
+      @change="activateControls"
+    >
       <v-carousel-item
         v-for="(image, i) in array"
         :key="i"
         reverse-transition="fade-transition"
         transition="fade-transition"
       >
-        <v-sheet height="100%" width="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <img :src="image" class="imageFit" />
-            <v-btn
-              style="bottom: 55px"
-              color="error"
-              fab
-              small
-              dark
-              absolute
-              bottom
-              right
-              @click="removeImage(i)"
+        <v-sheet height="100%" width="100%" absolute>
+          <v-container style="height: inherit">
+            <v-row
+              style="height: inherit"
+              class="ma-0"
+              align="center"
+              justify="center"
             >
-              <v-icon>mdi-delete</v-icon>
-            </v-btn>
-          </v-row>
+              <img :src="image" class="imageFit" />
+              <v-btn
+                style="bottom: 55px"
+                color="error"
+                fab
+                small
+                dark
+                absolute
+                bottom
+                right
+                @click="removeImage(i)"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-row>
+          </v-container>
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
   </v-dialog>
 </template>
+
+<style>
+.imageFit {
+  max-height: 100%;
+  max-width: 100%;
+}
+</style>
 
 <script>
 import { mapActions } from 'vuex'
@@ -39,9 +58,23 @@ export default {
   data() {
     return {
       dialog: false,
+      hideControls: false,
+      timer: undefined,
     }
   },
   methods: {
+    activateControls() {
+      console.log('hiding controls')
+      this.hideControls = false
+      if (this.timer !== undefined) window.clearTimeout(this.timer)
+      this.timer = setTimeout(
+        function () {
+          this.hideControls = true
+          console.log('hiding controls now')
+        }.bind(this),
+        2000
+      )
+    },
     removeImage(i) {
       if (this.array.length === 1) {
         this.dialog = false
