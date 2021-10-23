f61b3f52fba9730969a8cca7db1cadcece5c919b
AltFreq <altfreq07@gmail.com>
2021-10-23 20:34:26 +0000

    Categories on load button

--- components/Buttons/LoadCaseButton.vue
+++ components/Buttons/LoadCaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <v-menu offset-y rounded max-height="250px">
+    <v-menu offset-y rounded max-height="250px" :close-on-content-click="false">
       <template #activator="{ on, attrs }">
         <button-icon
           text="Load"
@@ -8,21 +8,29 @@
           :color="color"
           v-bind="attrs"
           v-on="on"
-          @click="log"
         />
       </template>
-      <v-list>
-        <v-list-item
-          v-for="(item, index) in getCases"
-          :key="index"
-          link
-          :to="'/case/' + item.id"
-        >
-          <v-list-item-title>{{
-            item.title === '' ? item.id : item.title
-          }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
+      <v-expansion-panels style="width: fit-content">
+        <v-expansion-panel v-for="(item, value, i) in getCategories" :key="i">
+          <v-expansion-panel-header>
+            {{ value === '' ? 'Uncategorised' : value }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list>
+              <v-list-item
+                v-for="(data, index) in item"
+                :key="index"
+                link
+                :to="'/case/' + data.id"
+              >
+                <v-list-item-title>{{
+                  data.title === '' ? data.id : data.title
+                }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-menu>
   </div>
 </template>
@@ -39,15 +47,24 @@ export default {
       default: '',
     },
   },
-  methods: {
-    log() {
-      console.log(this.getCases)
-    },
-  },
   computed: {
     ...mapGetters({
       getCases: 'cases/getCases',
     }),
+    getCategories() {
+      const categories = {}
+      for (const item of this.getCases) {
+        let category = item.category
+        if (category === undefined || category === '')
+          category = 'Uncategorised'
+        if (categories[category] === undefined) {
+          categories[category] = [item]
+        } else {
+          categories[category].push(item)
+        }
+      }
+      return categories
+    },
   },
 }
 </script>
