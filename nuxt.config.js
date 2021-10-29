236f4ef1bc23d34172fb38c3f3599068dd64dd8e
AltFreq <altfreq07@gmail.com>
2021-10-30 07:39:19 +1100

    try http2

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,4 +109,14 @@ export default {
   build: {
 
   },
+
+  render: {
+    http2: {
+      push: true,
+      pushAssets: (req, res, publicPath, preloadFiles) =>
+        preloadFiles
+          .filter(f => f.asType === 'script' && f.file === 'runtime.js')
+          .map(f => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`),
+    }
+  }
 }
