92647bad7d55e8df97fabf271a768f4e863ea2dd
AltFreq <altfreq07@gmail.com>
2021-11-02 20:50:02 +1100

    Reorder meta to first

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Logger',
-    title: 'Logger',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Multi-purpose customisable logging tool.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
+    titleTemplate: '%s - Logger',
+    title: 'Logger',
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
