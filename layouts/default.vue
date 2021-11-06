a18c7ca9641f6e18e9a2d7400dc15e5002105b9f
AltFreq <altfreq07@gmail.com>
2021-11-06 15:55:22 +1100

    Add some persistence protection

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,6 +21,17 @@ export default {
     }),
   },
   mounted() {
+    if (navigator.storage && navigator.storage.persist)
+      navigator.storage.persist().then(function (persistent) {
+        if (persistent)
+          console.log(
+            'Storage will not be cleared except by explicit user action'
+          )
+        else
+          console.log(
+            'Storage may be cleared by the UA under storage pressure.'
+          )
+      })
     if (this.getTheme === true) {
       return (this.$vuetify.theme.dark = true)
     } else {
